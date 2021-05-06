2d3a92cb18d26cc9158a163f54cf0bf1ed0cfb21
thisisstreamliner <thisisstreamliner@gmail.com>
2021-05-06 12:36:24 +0300

    Finish Calculation Logic for all operations

--- Calculator/Model/CalculatorLogic.swift
+++ Calculator/Model/CalculatorLogic.swift
@@ -12,20 +12,45 @@ struct CalculatorLogic {
     
     private var number: Double?
     
+    private var intermediateCalculation : (number: Double, calcMethod: String)?
+    
     mutating func setNumber(_ number : Double) {
         self.number = number
     }
     
-    func calculate(symbol: String) -> Double? {
+    mutating func calculate(symbol: String) -> Double? {
         if let n = number {
-            if symbol == "+/-" {
+            switch symbol {
+            case "+/-":
                 return n * -1
-            } else if symbol == "AC" {
+            case "AC":
                 return 0
-            } else if symbol == "%" {
+            case "%":
                 return n / 100
-            } else {
-                return 0
+            case "=":
+                performTwoNumberCalculation(number2: n)
+            default:
+                intermediateCalculation = (number: n, calcMethod: symbol)
+            }
+        }
+        return nil
+    }
+    
+    private func performTwoNumberCalculation(number2: Double) -> Double? {
+        
+        if let number1 = intermediateCalculation?.number,
+           let operation = intermediateCalculation?.calcMethod {
+            switch operation {
+            case "+":
+                return number1 + number2
+            case "-":
+                return number1 - number2
+            case "÷":
+                return number1 / number2
+            case "×":
+                return number1 * number2
+            default:
+                fatalError("Unknown operation")
             }
         }
         return nil
